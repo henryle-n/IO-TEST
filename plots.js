bf3b815d4b333c33a7720d1f089a374bf4c5f497
Henry Le <henryle.b11@gmail.com>
2024-01-04 20:52:06 -0600

    adding files

--- plots.js
+++ plots.js
@@ -2,11 +2,11 @@
 // The data from the JSON file is arbitrarily named importedData as the argument.
 d3.json("data/data.json").then((importedData) => {
   // console.log(importedData);
-  let data = importedData;
+  var data = importedData;
 
-  // Sort the data array by using the miles value.
+  // Sort the data array by using the greekSearchResults value.
   data.sort(function(a, b) {
-    return parseFloat(b.miles) - parseFloat(a.miles);
+    return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
   });
 
   // Slice the first 10 objects for plotting.
@@ -15,22 +15,22 @@ d3.json("data/data.json").then((importedData) => {
   // Reverse the array because of the Plotly defaults.
   data = data.reverse();
 
-  // Trace1 for the car miles data.
-  let trace1 = {
-    x: data.map(row => row.miles),
-    y: data.map(row => row.license_plate),
-    text: data.map(row => row.license_plate),
-    name: "Car Miles",
+  // Trace1 for the Greek data.
+  var trace1 = {
+    x: data.map(row => row.greekSearchResults),
+    y: data.map(row => row.greekName),
+    text: data.map(row => row.greekName),
+    name: "Greek",
     type: "bar",
     orientation: "h"
   };
 
   // Data
-  let chartData = [trace1];
+  var chartData = [trace1];
 
   // Apply the group bar mode to the layout.
-  let layout = {
-    title: "Car Miles for Employee Used Cars",
+  var layout = {
+    title: "Greek gods search results",
     margin: {
       l: 100,
       r: 100,
